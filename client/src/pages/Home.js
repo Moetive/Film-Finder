14ebfed1daec68759692a3e27e6aa621d1a673d2
Moetive <Moetive206@gmail.com>
2022-09-10 12:03:56 -0700

    adding API key and request object on home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,22 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-
-import ThoughtList from '../components/ThoughtList';
-import ThoughtForm from '../components/ThoughtForm';
-
-import { QUERY_THOUGHTS } from '../utils/queries';
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { API_URL, API_KEY } from "../components/config";
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const thoughts = data?.thoughts || [];
-
+  useEffect(() =>{
+    fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+    })
+  })
   return (
     <main>
       <div className="flex-row justify-center">
         <div
           className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          {/* <ThoughtForm /> */}
-        </div>
-        {/* <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
-          )}
-        </div> */}
+          style={{ border: "1px dotted #1a1a1a" }}
+        ></div>
       </div>
     </main>
   );
