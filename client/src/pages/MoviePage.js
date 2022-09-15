70c648ebb715fee5e85d024b17cbe37cf659463b
Moetive <Moetive206@gmail.com>
2022-09-15 13:32:00 -0500

    fixed banners and rows, also added titles to each movie

--- client/src/pages/MoviePage.js
+++ client/src/pages/MoviePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import {Card} from 'antd'
 export default function MoviePage() {
   return (
     <div>
-      
+      <Card title = "title" />
+        
+    
     </div>
   )
 }
