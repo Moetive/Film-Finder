1b2a2734a7b7bd497940cd6d35633a6db014023f
Moetive <Moetive206@gmail.com>
2022-09-15 17:33:54 -0500

    movie details page

--- client/src/pages/MoviePage.js
+++ client/src/pages/MoviePage.js
@@ -1,11 +1,25 @@
-import React from 'react'
-import {Card} from 'antd'
-export default function MoviePage() {
+import React, { useEffect } from 'react'
+import { API_URL, API_KEY } from '../components/config'
+// import { response } from 'express'
+
+function MoviePage(props) {
+  
+  useEffect(() => {
+    const movieId = props.match.params.movieId
+
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}/keywords?api_key=${API_KEY}`)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+    })
+  }, [])
+  
   return (
     <div>
-      <Card title = "title" />
+      
         
     
     </div>
   )
 }
+export default MoviePage
