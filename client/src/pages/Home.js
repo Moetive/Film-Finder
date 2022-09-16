fd548d7e120eaa1a2d6d854421b39e0a46a71453
Moetive <Moetive206@gmail.com>
2022-09-15 20:28:54 -0500

    Redundancy

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -65,7 +65,7 @@ const Home = () => {
                   }
                   movieId={movie.id}
                   title={movie.original_title}
-                  // text={movie.overview}
+                 
                 />
               </React.Fragment>
             ))}
