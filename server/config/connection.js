142f35f31d66eac71cf1c5bf49a2c27178d0e410
Moetive <Moetive206@gmail.com>
2022-09-16 15:56:57 -0500

    xxxyyzzz

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/BookSearchEngine',
+  process.env.MONGODB_URI || 'mongodb+srv://root:root@booksearchengine.nrwj6u3.mongodb.net/programming-thoughts?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
